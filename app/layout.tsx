7bac29af8cd27384021f903f8d47752180e54f06
Joanne Notley <49209412+jo1807@users.noreply.github.com>
2023-11-03 16:12:33 +0000

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Book Store',
+  description: 'Display book store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
